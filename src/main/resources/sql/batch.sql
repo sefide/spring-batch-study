c0916d2e2b736c2123de41b22db965a24958c116
sefide <sefide@naver.com>
2021-03-20 23:09:29 +0900

    feat: add batch sql

---
+++ src/main/resources/sql/batch.sql
@@ -0,0 +1,11 @@
+## Job 실행 기록 - 고유한 JobParamter만 기록
+SELECT * FROM BATCH_JOB_INSTANCE;
+
+## Job 실행(JOB_INSTANCE)의 성공/실패 내역 기록
+SELECT * FROM BATCH_JOB_EXECUTION;
+
+## JOB_EXECUTION에 따른 Job Parameter 기록
+SELECT * FROM BATCH_JOB_EXECUTION_PARAMS;
+
+## Job내에 위치한 Step 실행 기록
+SELECT * FROM BATCH_STEP_EXECUTION;
